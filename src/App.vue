c094b57f61454ab5e222c390e7acc4e752dd7032
X3R0 <x3r0@outlook.fr>
2025-01-26 16:59:09 +0100

    Setting up a temporary website

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="coming-soon">
+    <h1>{{ title }}</h1>
+    <p>{{ message }}</p>
+    <a :href="discordLink.url" target="_blank">{{ discordLink.text }}</a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    return {
+      title: '',
+      message: '',
+      discordLink: {
+        url: 'https://discord.gg/ryjcK2trHd',
+        text: '',
+      },
+    };
+  },
+  methods: {
+    detectLanguage(): string {
+      const userLang = navigator.language || navigator.userLanguage;
+      return userLang.substring(0, 2);
+    },
+    updateContent(language: string) {
+      const titles = {
+        fr: 'HyTools',
+        en: 'HyTools',
+      };
+
+      const messages = {
+        fr: 'Notre site est en cours de développement. Rejoignez notre communauté sur Discord pour rester informé.',
+        en: 'Our website is under development. Join our Discord community to stay informed.',
+      };
+
+      const links = {
+        fr: 'Rejoignez-nous sur Discord',
+        en: 'Join us on Discord',
+      };
+
+      this.title = titles[language] || titles['en'];
+      this.message = messages[language] || messages['en'];
+      this.discordLink.text = links[language] || links['en'];
+    },
+  },
+  mounted() {
+    const language = this.detectLanguage();
+    this.updateContent(language);
+  },
+});
+</script>
+
